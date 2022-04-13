2380a0c7a95b6916895080e6738c2c6e810bb126
Fidan <fidan.akhmed.anar98@gmail.com>
2022-04-13 16:51:51 +0400

    написать КЛАСС Background, изменяющий цвет канвы в зависимости от времени в приложении * реализовать добавление новых кружков по клику, используя ТОЛЬКО массивы * реализовать по клику другой кнопки удаление кружков

--- src/main/java/java2/GameCanvas.java
+++ src/main/java/java2/GameCanvas.java
@@ -43,7 +43,7 @@ public class GameCanvas extends JPanel{
                        System.arraycopy(MainCircles.balls, 0, newBalls, 0, MainCircles.balls.length - 1);
                        MainCircles.balls = newBalls;
                        MainCircles.count--;
-                       System.out.println("-- = " + MainCircles.balls.length);
+                       //  System.out.println("-- = " + MainCircles.balls.length);
                }
             }
         });
